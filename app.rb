9b3d2c62ab5564a89201cd4260c5ea886460f133
Anastasia M <anastasiamaschenko103@gmail.com>
2021-06-05 23:05:59 +0300

    Add movies of director queries

--- app.rb
+++ app.rb
@@ -4,22 +4,27 @@ class DB
   def initialize()
     @driver = Neo4j::Driver::GraphDatabase.driver(ENV['NEO4J_URI'], Neo4j::Driver::AuthTokens.basic(ENV['NEO4J_USER'], ENV['NEO4J_PASSWORD']))
   end
+
+  def movies_of_director(director)
+    @driver.session { |session| session.run('MATCH (movie:MOVIE)=[:DIRECTED]-(:Person {name: $name}) RETURN movie.title', director: director) }
+  end
 end
 
 def handle_input(input)
+  db = DB.new()
   case input
   in ['director', director]
-    "Here will be films of director #{director}"
-  in ['collegues', person]
+    db.movies_of_director(director)
+  in ['colleagues', person]
     "Here will be all people who worked with #{person}"
-  in ['actors_count', film]
-    "Here will be count of actors who took part in #{film}"
+  in ['actors_count', movie]
+    "Here will be count of actors who took part in #{movie}"
   in ['help']
     """
     Flags for app.rb:
-    director <director> -- finds all films of director
-    collegues <person> -- finds all people who worked with person
-    actors_count -- how many actors took part in film
+    director <director> -- finds all movies of director
+    colleagues <person> -- finds all people who worked with person
+    actors_count <movie> -- how many actors took part in movie
     help -- puts help
     """
   else
