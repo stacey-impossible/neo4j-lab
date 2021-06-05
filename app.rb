83f5eadb86923c51e89986d07844159e1699c546
Anastasia M <anastasiamaschenko103@gmail.com>
2021-06-05 17:30:34 +0300

    Base structure of app.rb script

--- app.rb
+++ app.rb
@@ -1,2 +1,23 @@
-puts "Here will be app!"
+def handle_input(input)
+  case input
+  in ['director', director]
+    "Here will be films of director #{director}"
+  in ['collegues', person]
+    "Here will be all people who worked with #{person}"
+  in ['actors_count', film]
+    "Here will be count of actors who took part in #{film}"
+  in ['help']
+    """
+    Flags for app.rb:
+    director <director> -- finds all films of director
+    collegues <person> -- finds all people who worked with person
+    actors_count -- how many actors took part in film
+    help -- puts help
+    """
+  else
+    'Wrong command. Print "ruby app.rb help" for help.'
+  end
+end
+
+puts handle_input(ARGV)
 
